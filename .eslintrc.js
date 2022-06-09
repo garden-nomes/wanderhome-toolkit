3249dd22971d20a7cb003c6425f8ed3eea78744d
Naomi Nesbit-Weiner <naomibweiner@gmail.com>
2022-06-09 12:29:47 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+  ],
+};
