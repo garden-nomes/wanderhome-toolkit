cdfe1829c67e804286164c9451d32cda850f2be6
Naomi Nesbit-Weiner <naomibweiner@gmail.com>
2022-07-27 16:02:01 -0700

    clean up editable list UI

--- src/components/EditableList.vue
+++ src/components/EditableList.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { isEqual } from "lodash";
-import { ref, watch } from "vue";
+import { nextTick, ref, watch } from "vue";
 import uid, { UID } from "../lib/uid";
 import EditableText from "./EditableText.vue";
 import Icon from "./Icon.vue";
@@ -51,10 +51,25 @@ const removeItem = (id: UID) => {
   localModel.value = localModel.value.filter((item) => item.id !== id);
 };
 
-const addItem = (value: string) => {
+const isAddTextEditing = ref(false);
+const addItem = async (value: string) => {
   if (value.length) {
     const id = uid();
     localModel.value = [...localModel.value, { id, value }];
+
+    await nextTick();
+    isAddTextEditing.value = true;
+  }
+};
+
+const updateItem = (id: UID, value: string) => {
+  value = value.trim();
+
+  if (value.length) {
+    const index = localModel.value.findIndex((item) => item.id === id);
+    localModel.value[index].value = value;
+  } else {
+    removeItem(id);
   }
 };
 </script>
@@ -69,28 +84,34 @@ const addItem = (value: string) => {
       <li
         v-for="item in localModel"
         :key="item.id"
-        class="d-flex mb-1"
+        class="d-flex mb-1 position-relative"
       >
         <editable-text
-          v-model="item.value"
-          label="item"
+          :model-value="item.value"
+          label="Item"
+          hide-label
           class="flex-grow-1"
+          @update:model-value="updateItem(item.id, $event)"
         />
 
-        <div class="flex-shrink-1">
+        <div class="flex-shrink-1 position-absolute end-0">
           <button
             type="button"
-            class="btn btn-link text-secondary w-100"
+            class="btn btn-minimal visually-hidden-focusable"
             aria-label="remove"
             @click="removeItem(item.id)"
           >
-            <icon name="trash" />
+            <icon
+              name="dash"
+              class="me-1"
+            />Remove
           </button>
         </div>
       </li>
 
       <li>
         <editable-text
+          v-model:is-editing="isAddTextEditing"
           model-value=""
           :label="addLabel"
           @update:model-value="addItem"
