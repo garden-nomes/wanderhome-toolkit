2feb12ee8a07815d917a5ea3067a0fc1abb7a551
Naomi Nesbit-Weiner <naomibweiner@gmail.com>
2022-07-27 16:01:31 -0700

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup>
 import SheetsToolbar from "./components/SheetsToolbar.vue";
+import AppFooter from "./components/AppFooter.vue";
 </script>
 
 <template>
-  <sheets-toolbar />
+  <div class="min-vh-100 d-flex flex-column">
+    <sheets-toolbar />
 
-  <div class="container py-3">
-    <router-view />
+    <div class="container py-3">
+      <router-view />
+    </div>
+
+    <app-footer class="mt-auto" />
   </div>
 </template>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="container text-center pb-3">
+    <div class="row justify-content-center">
+      <div class="col-12 col-lg-5">
+        <p class="small text-secondary mb-1">
+          Wanderhome is copyright of
+          <a
+            href="https://possumcreekgames.com/pages/wanderhome-3rd-party-license"
+            target="_blank"
+          >Possum Creek Games Inc</a>.
+        </p>
+
+        <p class="small text-secondary mb-0">
+          Wanderhome Toolkit is an independent production and is not affiliated
+          with Possum Creek Games Inc. It is published under the
+          <a
+            href="https://possumcreekgames.com/pages/wanderhome-3rd-party-license"
+            target="_blank"
+          >Wanderhome Third Party License</a>.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
